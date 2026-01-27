c490e5484015d3c59b3cf27884dde9de6640c87a
Daniel Barbagallo <debarbagallo@yahoo.com.ar>
2026-01-27 01:56:16 -0300

    Arregle el struct, y cree prueba para ver si sube

--- struct1/src/main.rs
+++ struct1/src/main.rs
@@ -1,25 +1,31 @@
 /*prueba de struct, como funciona */
 
+#[derive(Debug)]
+struct Persona {
+    nombre: String,
+    apellido: String,
+    edad: u8,
+    telefono: String,
+    domicilio: String,
+    genero: char,
+    estado_civil: char,
+}
+
 fn main() {
-    struct Persona {
-        nombre: String,
-        apellido: String,
-        edad: u8,
-        telefono: String,
-        domicilio: String,
-        genero: char,
-        estado_civil: char,
+    let mut persona1: Persona = Persona {
+        nombre: String::from("Pepe"),
+        apellido: String::from("Garcia"),
+        edad: 47,
+        telefono: String::from("123456"),
+        domicilio: String::from("calle falsa 123"),
+        genero: 'M',
+        estado_civil: 'S',
+    };
+
+    if persona1.nombre == "Juan" {
+        println!("Hola {}", persona1.nombre);
     }
+
+    persona1.nombre = String::from("Esteban");
+    println!("La Persona es: {:#?}", persona1);
 }
-
-let mut persona1 =Persona;
-persona1.nombre="Pepe";
-persona1.apellido="Garcia";
-persona1.edad=47;
-persona1.telefono="123456";
-
-if persona1.nombre=="Juan" {
-    println!("Hola {}", persona1.nombre);
-}
-
-persona1.nombre="Esteban";
