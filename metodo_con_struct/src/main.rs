91ef7ba81cb2cd71c808a1a863c5131645ca27b5
Daniel Barbagallo <debarbagallo@yahoo.com.ar>
2026-01-30 00:24:07 -0300

    seguimos con struct definiendo metodos en sus implementaciones

--- metodo_con_struct/src/main.rs
+++ metodo_con_struct/src/main.rs
@@ -19,13 +19,26 @@ impl Cuadrado {
     fn volunen(&self) -> u32 {
         self.lado * self.lado * self.lado
     }
+
+    fn get_lado(&self) -> u32 {
+        self.lado
+    }
+
+    fn set_lado(&mut self, lado: u32) {
+        self.lado = lado;
+    }
 }
 
 fn main() {
-    let cuadrado = Cuadrado { lado: 10 };
+    let mut cuadrado = Cuadrado { lado: 10 };
     println!("\nEl area del cuadrado es: {}", cuadrado.area());
     println!("El perimetro del cuadrado es: {}", cuadrado.perimetro());
     println!("El volunen del cuadrado es: {}", cuadrado.volunen());
 
     println!("\n {cuadrado:?}");
+    println!("El lado del cuadrado es: {}", cuadrado.get_lado());
+    cuadrado.set_lado(21);
+    println!("\n {cuadrado:?}");
+    println!("El lado del cuadrado es: {}", cuadrado.get_lado());
 }
+
