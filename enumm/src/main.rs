0fdb64557894d4d201d963f0669ffa1ec8e8d47a
Daniel Barbagallo <debarbagallo@yahoo.com.ar>
2026-01-31 19:52:33 -0300

    viendo enumm x manual web

--- enumm/src/main.rs
+++ enumm/src/main.rs
@@ -1,27 +1,27 @@
 /* estoy probando los enum, con un ejemplo de componentes de una Pc*/
 
 enum ComponentesPc {
-    Teclado,
-    Mouse,
-    Parlantes,
+    Teclado(String),
+    Mouse(String),
+    Parlantes(String),
     Monitor,
     Cpu,
     Ram,
 }
 
-fn Un_Componente(componente:ComponentesPc){
+fn Un_Componente(componente: ComponentesPc) {
     match componente {
-        ComponentesPc::Teclado=>println!("es un Teclado"),
-        _=>println!("es otro componente"),
+        ComponentesPc::Teclado(_) => println!("es un Teclado"),
+        _ => println!("es otro componente"),
     }
 }
 
 fn main() {
-    let mon_1=ComponentesPc::Monitor;
-    let cpu_1=ComponentesPc::Cpu;
-    let teclado_1=ComponentesPc::Teclado;
+    let mon_1 = ComponentesPc::Monitor;
+    let cpu_1 = ComponentesPc::Cpu;
+    let teclado_1 = ComponentesPc::Teclado;
     Un_Componente(mon_1);
-    Un_Componente(teclado_1);
+    Un_Componente(teclado_1(String::from("Hp 104 teclas")));
     Un_Componente(cpu_1);
 
     //println!("Hello, world!");
