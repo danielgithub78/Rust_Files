4df5eb1f3314e1194a37fdbbb2f7a22449eb323d
Daniel Barbagallo <debarbagallo@yahoo.com.ar>
2026-02-22 23:36:00 -0300

    quite pagos::procesar_tarjeta(precio) del main, porque cobraba 2 veces al cliente, ya estaba definido en compras/mod.rs en la funcion realizar_compra()

--- tienda/src/compras/pagos.rs
+++ tienda/src/compras/pagos.rs
@@ -2,6 +2,6 @@ pub fn procesar_tarjeta(monto:u32){
     println!("Procesando pago de ${}, con tarjeta...", monto);
 }
 
-fn validar_pin(){
+fn validar_pin(){ //solamente disponible aca. Nadie mas la ve, es privada
     println!("PIN validado.");
 }
--- tienda/src/main.rs
+++ tienda/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let precio=1500;
 
     if crate::inventario::consultar_stock(producto){
-        pagos::procesar_tarjeta(precio);
+        // pagos::procesar_tarjeta(precio); si dejo este le estariamos cobrando 2 veces al cliente
         compras::realizar_compra(producto, precio);
     }
     //println!("Hello, world!");
